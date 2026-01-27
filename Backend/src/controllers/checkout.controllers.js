0b211fd7fec6796d05b8696a30ab9aad80621021
Shiva Pandit <shivakrwpa1@gmail.com>
2026-01-27 18:40:41 +0530

    Implement updateCheckout functionality to handle payment status updates and enhance checkout session management

--- Backend/src/controllers/checkout.controllers.js
+++ Backend/src/controllers/checkout.controllers.js
@@ -4,7 +4,7 @@ import Cart from "../models/cart.model.js";
 import Order from "../models/order.model.js";
 import httpsstatus from "http-status";
 
-const checkoutSessions = async (req, res) => {
+export const checkoutSessions = async (req, res) => {
   const { checkoutItems, shippingAddress, paymentMethod, totalPrice } =
     req.body;
   if (!checkoutItems || checkoutItems.length === 0) {
@@ -27,6 +27,36 @@ const checkoutSessions = async (req, res) => {
     res.status(httpsstatus.CREATED).json(newCheckout);
   } catch (error) {
     console.error("Error creating checkout session:", error);
+    res
+      .status(httpsstatus.INTERNAL_SERVER_ERROR)
+      .json({ message: "Server Error" });
+  }
+};
+
+export const updateCheckout = async (req, res) => {
+  const { paymentStatus, paymentDetails } = req.body;
+  try {
+    const checkout = await CheckOut.findById(req.params.id);
+    if (!checkout) {
+      return res
+        .status(httpsstatus.NOT_FOUND)
+        .json({ message: "checkout not found" });
+    }
+    if (paymentStatus === "paid") {
+      checkout.isPaid = true;
+      checkout.paymentStatus = paymentStatus;
+      checkout.paymentDetails = paymentDetails;
+      checkout.paidAt = Date.now();
+      await checkout.save();
+
+      res.status(httpsstatus.OK).json(checkout);
+    } else {
+      res
+        .status(httpsstatus.BAD_REQUEST)
+        .json({ message: "Invalid payment status" });
+    }
+  } catch (error) {
+    console.error(error);
     res
       .status(httpsstatus.INTERNAL_SERVER_ERROR)
       .json({ message: "Server Error" });
--- Backend/src/routes/checkout.routes.js
+++ Backend/src/routes/checkout.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import isAuth from "../middlewares/isAuth.js";
+import {
+  checkoutSessions,
+  updateCheckout,
+} from "../controllers/checkout.controllers.js";
 
 const router = Router();
 
-router.post("/", isAuth);
+router.post("/", isAuth, checkoutSessions);
+router.put("/:id/pay", isAuth, updateCheckout);
 
 export default router;
