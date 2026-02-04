f18589c021f9686417ebe244171d2ff06173c6c7
Shiva Pandit <shivakrwpa1@gmail.com>
2026-02-04 21:49:52 +0530

    Done with Admin Product Slice

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -5,6 +5,7 @@ import cartReducer from "./slices/cartSlice";
 import checkoutReducer from "./slices/checkoutSlice";
 import orderReducer from "./slices/orderSlice";
 import adminReducer from "./slices/adminSlice";
+import adminProductReducer from "./slices/adminProductSlice";
 
 const store = configureStore({
   reducer: {
@@ -14,6 +15,7 @@ const store = configureStore({
     checkout: checkoutReducer,
     orders: orderReducer,
     admin: adminReducer,
+    adminProduct: adminProductReducer,
   },
 });
 
