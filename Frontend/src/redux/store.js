c4ca3e1bcc4ae2204d54abf47a024e5a8432e23f
Shiva Pandit <shivakrwpa1@gmail.com>
2026-02-04 12:21:11 +0530

    Done with checkout Slice

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -2,11 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartSlice";
+import checkoutReducer from "./slices/checkoutSlice";
+
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
     cart: cartReducer,
+    checkout: checkoutReducer,
   },
 });
 
